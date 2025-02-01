5fb5bb83964d7f5fc8537ec1d0423bc365148b4b
Aysha-Mohammed <ayshamohammed1443@gmail.com>
2025-02-01 23:49:46 +0600

    Add files via upload

---
+++ selection sort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main() {
+int n;
+cout<<"enter n: ";
+cin>>n;
+int arr[n];
+cout<<"enter all the elements: ";
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+  }
+  for(int i=0;i<n;i++)
+  {
+    int min=i;
+    for(int j=i+1;j<n;j++)
+    {
+       if(arr[j]<arr[min])
+       {
+           min=j;
+       }
+       if(arr[min]!=i)
+       {
+           swap
+           (arr[i], arr[min]);
+       }
+    }
+  }
+  cout<<"sorted elements: ";
+  for(int i=0;i<n;i++)
+  {
+      cout<<arr[i]<<" ";
+  }
+}
